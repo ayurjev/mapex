96b33df702f75d9d9f7fc8a21aadfed018735136
ayurjev <andrey.yurjev@gmail.com>
2014-02-11 17:20:19 +0400

    Исправлено использование or и and в запросах на выборку

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+from mapex.core.Sql import Adapter as DatabaseClient
 from mapex.core.Mappers import SqlMapper, NoSqlMapper
 from mapex.core.Models import TableModel as CollectionModel, RecordModel as EntityModel
 from mapex.dbms.Adapters import PgSqlDbAdapter as PgSqlClient, MySqlDbAdapter as MySqlClinet, \
