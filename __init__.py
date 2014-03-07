4c8ba4d7368d7b19314155ee31773e6ff2ad5eff
ayurjev <andrey.yurjev@gmail.com>
2014-03-07 12:06:27 +0400

    Добавлено импортирование классов EmbeddedObject и EmbeddedFactory в основной __init__ mapex'a

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from mapex.core.Sql import Adapter as DatabaseClient
 from mapex.core.Mappers import SqlMapper, NoSqlMapper
 from mapex.core.Models import TableModel as CollectionModel, RecordModel as EntityModel
+from mapex.core.Models import EmbeddedObject, EmbeddedObjectFactory
 from mapex.dbms.Adapters import PgSqlDbAdapter as PgSqlClient, MySqlDbAdapter as MySqlClinet, \
     MsSqlDbAdapter as MsSqlClient, MongoDbAdapter as MongoClient
 
