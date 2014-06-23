41b7246f7611799fd31a21e3682c6352b1580f48
Niev <evselyev@yandex.ru>
2014-06-23 18:52:53 +0400

    утилита merge_dict() с тестами для неё и для do_dict()

---
+++ tests/utils_tests.py
@@ -0,0 +1,46 @@
+""" Тесты библиотеки утилит """
+import unittest
+from mapex.utils import merge_dict, do_dict
+
+
+class UtilsTests(unittest.TestCase):
+    """ Юниттесты утилит """
+    def test_do_dict(self):
+        """ Утилита do_dict конструирует словарь по точечной нотации поля """
+        self.assertDictEqual({"a": {"b": {"c": 1}}}, do_dict("a.b.c", 1))
+
+    def test_merge_dict(self):
+        """ Утилита merge_dict корректно объединяет словари """
+
+        # Случай когда у словарей нет общих ключей
+        self.assertDictEqual(
+            {
+                "a": 1,
+                "b": 2
+            },
+            merge_dict({"a": 1}, {"b": 2})
+        )
+
+        # Случай когда у словарей есть общий ключ. Обновляется лишь часть данных
+        self.assertDictEqual(
+            {
+                "a": {
+                    "c": 3,
+                    "b": 2
+                }
+            },
+            merge_dict({"a": {"c": 3}}, {"a": {"b": 2}})
+        )
+
+        # merge_dict() на вход принимает один словарь приёмник и неограниченное количество словарей источников
+        self.assertDictEqual(
+            {
+                "a": 1,
+                "b": 2,
+                "c": 3
+            },
+            merge_dict({}, {"a": 1}, {"b": 2}, {"c": 3})
+        )
+
+if __name__ == "__main__":
+    unittest.main()
--- utils.py
+++ utils.py
@@ -13,10 +13,25 @@ def partition(predicate, iterable):
 
 
 def do_dict(notation, value) -> dict:
-    """ Рекурсивно строит многомерный словарь по предоставленной точечной нотации
+    """
+    Рекурсивно строит многомерный словарь по предоставленной точечной нотации
     @param notation: точечная нотация (Пример: user.profile.email)
     @param value: значение поля (Пример: test@test.com)
     @return: словарь с записанным в него значением value (Пример: {"user": {"profile": {"email": "test@test.com"}}})
     """
     head, _, tail = notation.partition(".")
     return {head: do_dict(tail, value)} if tail else {head: value}
+
+
+def merge_dict(dest: dict, *sources: dict) -> dict:
+    """
+    Рекурсивно обновляет словарь приёмник данными из словарей источников
+    @param dest: словарь приёмник
+    @param sources: словари источники
+    @return:
+    """
+    for source in sources:
+        for key, value in source.items():
+            dest[key] = merge_dict(dest[key] if key in dest.keys() else {}, value) if isinstance(value, dict) else value
+
+    return dest
